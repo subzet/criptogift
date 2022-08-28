940c86273b74b0cf140055693aa3b1e87590920c
Manuel Ponsa <manuponsa@gmail.com>
2022-08-28 20:08:49 -0300

    correct deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const CrowfundContract = await ethers.getContractFactory("Lock");
+  const CrowfundContract = await ethers.getContractFactory("Crowfund");
   const crowfund = await CrowfundContract.deploy();
 
   await crowfund.deployed();
 
-  console.log(`CriptoGift deployed`);
+  console.log(`CriptoGift deployed to ${crowfund.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
